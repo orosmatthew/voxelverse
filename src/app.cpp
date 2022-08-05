a252de48c848c869404f3ae6f317c8bf107a8369
orosmatthew <orosmatthew@pm.me>
2022-08-04 21:00:44 -0400

    Update log level

--- src/app.cpp
+++ src/app.cpp
@@ -22,68 +22,68 @@ namespace app
 {
     void run()
     {
-        spdlog::info("Creating window");
+        spdlog::debug("Creating window");
         mve::UniqueGlfwWindow window = mve::createWindow(800, 600, "Mini Vulkan Engine");
 
-        spdlog::info("Creating Vulkan instance");
+        spdlog::debug("Creating Vulkan instance");
         vk::UniqueInstance instance = mve::createInstance("Hello Vulkan", 1);
 
-        spdlog::info("Creating Vulkan surface");
+        spdlog::debug("Creating Vulkan surface");
         vk::UniqueSurfaceKHR surface = mve::createSurface(window, instance);
 
-        spdlog::info("Getting Vulkan physical device");
+        spdlog::debug("Getting Vulkan physical device");
         vk::PhysicalDevice physicalDevice = mve::getBestPhysicalDevice(instance, surface);
 
-        spdlog::info("Creating Vulkan logical device");
+        spdlog::debug("Creating Vulkan logical device");
         vk::UniqueDevice device = mve::createDevice(surface, physicalDevice);
 
-        spdlog::info("Getting Vulkan graphics Queue");
+        spdlog::debug("Getting Vulkan graphics Queue");
         vk::Queue graphicsQueue = mve::getGraphicsQueue(surface, physicalDevice, device);
 
-        spdlog::info("Getting Vulkan present Queue");
+        spdlog::debug("Getting Vulkan present Queue");
         vk::Queue presentQueue = mve::getPresentQueue(surface, physicalDevice, device);
 
-        spdlog::info("Getting bet Vulkan surface format");
+        spdlog::debug("Getting bet Vulkan surface format");
         vk::SurfaceFormatKHR surfaceFormat = mve::getBestSurfaceFormat(surface, physicalDevice);
 
-        spdlog::info("Getting best Vulkan extent");
+        spdlog::debug("Getting best Vulkan extent");
         vk::Extent2D extent = mve::getBestExtent(window, surface, physicalDevice);
 
-        spdlog::info("Creating Vulkan swapchain");
+        spdlog::debug("Creating Vulkan swapchain");
         vk::UniqueSwapchainKHR swapchain = mve::createSwapchain(surface, physicalDevice, device, surfaceFormat, extent);
 
-        spdlog::info("Getting Vulkan swapchain Images");
+        spdlog::debug("Getting Vulkan swapchain Images");
         std::vector<vk::Image> swapchainImages = mve::getSwapchainImages(device, swapchain);
 
-        spdlog::info("Getting Vulkan image views");
+        spdlog::debug("Getting Vulkan image views");
         std::vector<vk::UniqueImageView> imageViews = mve::createImageViews(device, surfaceFormat, swapchainImages);
 
-        spdlog::info("Creating Vulkan vertex shader module");
+        spdlog::debug("Creating Vulkan vertex shader module");
         vk::UniqueShaderModule vertexShaderModule = mve::createShaderModule(
             device, "C:/dev/vulkan_testing/shaders/simple.vert", mve::ShaderType::eVertex, true);
 
-        spdlog::info("Creating Vulkan fragment shader module");
+        spdlog::debug("Creating Vulkan fragment shader module");
         vk::UniqueShaderModule fragmentShaderModule = mve::createShaderModule(
             device, "C:/dev/vulkan_testing/shaders/simple.frag", mve::ShaderType::eFragment, true);
 
-        spdlog::info("Creating Vulkan render pass");
+        spdlog::debug("Creating Vulkan render pass");
         vk::UniqueRenderPass renderPass = mve::createRenderPass(device, surfaceFormat);
 
-        spdlog::info("Creating Vulkan graphics pipeline");
+        spdlog::debug("Creating Vulkan graphics pipeline");
         vk::UniquePipeline graphicsPipeline
             = mve::createGraphicsPipeline(device, extent, vertexShaderModule, fragmentShaderModule, renderPass);
 
-        spdlog::info("Creating Vulkan frame buffers");
+        spdlog::debug("Creating Vulkan frame buffers");
         std::vector<vk::UniqueFramebuffer> framebuffers
             = mve::createFramebuffers(device, extent, imageViews, renderPass);
 
-        spdlog::info("Creating Vulkan command pool");
+        spdlog::debug("Creating Vulkan command pool");
         vk::UniqueCommandPool commandPool = mve::createCommandPool(physicalDevice, device, surface);
 
-        spdlog::info("Creating Vulkan command buffer");
+        spdlog::debug("Creating Vulkan command buffer");
         vk::UniqueCommandBuffer commandBuffer = mve::createCommandBuffer(device, commandPool);
 
-        spdlog::info("Entering main loop");
+        spdlog::debug("Entering main loop");
         mainLoop(window);
     }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,13 @@
 #include <iostream>
 
 int main() {
+
+#ifdef NDEBUG
+    spdlog::set_level(spdlog::level::info);
+#else
+    spdlog::set_level(spdlog::level::debug);
+#endif
+
     try {
         app::run();
     } catch (const std::exception& e) {
