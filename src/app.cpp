14acb274d8dbee1a789b3a0ed873ae8ab5e3bb50
orosmatthew <orosmatthew@pm.me>
2022-11-17 15:15:17 -0500

    Remove using namespace mve

--- src/app.cpp
+++ src/app.cpp
@@ -6,26 +6,24 @@
 
 #include "window.hpp"
 
-using namespace mve;
-
 namespace app {
 
     void run()
     {
         LOG->debug("Creating window");
 
-        auto window = Window("Mini Vulkan Engine", glm::ivec2(800, 600));
+        auto window = mve::Window("Mini Vulkan Engine", glm::ivec2(800, 600));
 
-        auto vertex_shader = Shader("../shaders/simple.vert", ShaderType::e_vertex, true);
-        auto fragment_shader = Shader("../shaders/simple.frag", ShaderType::e_fragment, true);
+        auto vertex_shader = mve::Shader("../shaders/simple.vert", mve::ShaderType::e_vertex, true);
+        auto fragment_shader = mve::Shader("../shaders/simple.frag", mve::ShaderType::e_fragment, true);
 
-        auto renderer = Renderer(window, "Vulkan Testing", 0, 0, 1, vertex_shader, fragment_shader);
+        auto renderer = mve::Renderer(window, "Vulkan Testing", 0, 0, 1, vertex_shader, fragment_shader);
 
         window.set_resize_callback([&](glm::ivec2 new_size) { renderer.draw_frame(window); });
 
         auto vertex_layout = mve::VertexLayout();
-        vertex_layout.push_back(VertexAttributeType::e_vec2); // 2D position
-        vertex_layout.push_back(VertexAttributeType::e_vec3); // Color
+        vertex_layout.push_back(mve::VertexAttributeType::e_vec2); // 2D position
+        vertex_layout.push_back(mve::VertexAttributeType::e_vec3); // Color
 
         auto big_triangle = mve::VertexData(vertex_layout);
         big_triangle.add_data({ 0.0f, -0.5f });
@@ -58,7 +56,7 @@ namespace app {
         while (!window.should_close()) {
             window.update();
 
-            if (window.get_key(InputKey::e_escape)) {
+            if (window.get_key(mve::InputKey::e_escape)) {
                 break;
             }
 
